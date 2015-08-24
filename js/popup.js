89c7859d3cb01a24e187378635f09ef6e6f2308e
Jonas Lomholdt <jonaslomholdt@gmail.com>
2015-08-24 21:48:12 +0200

    Minor cleanup of popup.js

--- js/popup.js
+++ js/popup.js
@@ -345,18 +345,3 @@ update();
 document.onscroll = function () {
 	is_scrolling = true;
 }
-
-
-
-
-// Like the new design
-/*
-var APP_ID = 267128753314780;
-var URL_TO_LIKE = "https://www.facebook.com/pages/Auto-Refresh-Plus-Chrome-Extension/351842721561492";
-
-var parent = document.getElementById("fb-like");
-var src = "http://www.facebook.com/plugins/like.php?href=" + encodeURIComponent(URL_TO_LIKE) + 
-          "&amp;send=false&amp;layout=button_count&amp;width=60&amp;show_faces=false&amp;font&amp;" + 
-          "colorscheme=light&amp;action=like&amp;height=90&amp;appId=" + APP_ID;
-parent.innerHTML = '<iframe src="'+ src +'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:90px; height:21px;" allowTransparency="true"><\/iframe>';
-*/
